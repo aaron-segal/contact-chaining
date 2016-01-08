6160192f760c6bc442c64f1f76893511625e2e06
aaron-segal <aaron.segal@yale.edu>
2016-01-08 17:43:16 -0500

    Set test scripts to quiet mode

--- oversight.sh
+++ oversight.sh
@@ -6,7 +6,7 @@ for d in 25 50 75 100 150 200
 do
 for x in 1 2 3 4 5
 do
-java cc.OversightAgency ../tests/lognorm/conf/agencies.conf -c ../tests/lognorm/conf/agency$1.conf -d ${d} -l ${l}
+java cc.OversightAgency ../tests/lognorm/conf/agencies.conf -c ../tests/lognorm/conf/agency$1.conf -d ${d} -l ${l} -q
 sleep 1
 done
 done
